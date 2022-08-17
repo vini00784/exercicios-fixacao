e11234e793ef84c3a456f7a340520bacd0fe3f70
Vinicius Santos <vo70346@gmail.com>
2022-08-17 09:37:30 -0300

    inicio exercício B

---
+++ assets/js/b.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function showNumber() {
+    const number = document.getElementById('number').value
+    const form = document.getElementById('form')
+    const result = document.getElementById('result')
+
+}
