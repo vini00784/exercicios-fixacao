b887c563dc37b8abf9555023047f83ee4b639f08
Vinicius Santos <vo70346@gmail.com>
2022-08-17 10:15:50 -0300

    exercicio B finalizado (no style)

--- assets/js/b.js
+++ assets/js/b.js
@@ -3,11 +3,16 @@
 function showNumber() {
     const number = document.getElementById('number').value
     const form = document.getElementById('form')
-    const result = document.getElementById('result')
+    const showNumber = document.getElementById('result')
 
     if(form.reportValidity()) {
         if(number > 0) {
-            result.textContent
+            showNumber.textContent = number
+        }else if(number < 0) {
+            showNumber.textContent = (number * (-1))
         }
     }
 }
+
+document.getElementById('showNumber')
+        .addEventListener('click', showNumber)
