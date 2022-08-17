04857ad67ed3921d218ac28aba7aad50b4a05bb3
Vinicius Santos <vo70346@gmail.com>
2022-08-17 11:04:12 -0300

    exercicio C finalizado (no style)

--- assets/js/c.js
+++ assets/js/c.js
@@ -7,14 +7,26 @@ function AverageCalculate() {
     const n4 = document.getElementById('number4').value
     const form = document.getElementById('form')
     const result = document.getElementById('result')
+    let studentStatus
+    let average
 
     if(form.reportValidity()) {
         if (n1 > 10 || n1 < 1 || n2 > 10 || n2 < 1 || n3 > 10 || n3 < 1 || n4 > 10 || n4 < 1) {
             alert('ERRO! Os números devem estar entre 1 e 10!')
         } else {
-            let average = (n1 + n2 + n3 + n4) / 4
-            result.textContent = average
+            average = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3) + parseFloat(n4)) / 4
+
+            if(average >= 7) {
+                studentStatus = 'APROVADO!'
+            } else if(average >=5 && average <= 6.9) {
+                studentStatus = 'DE EXAME!'
+            } else {
+                studentStatus = 'REPROVADO!'
+            }
         }
+
+        result.textContent = `Sua média é ${average.toFixed(2)} e você está ${studentStatus}`
+        console.log('VASCO')
     }
 }
 document.getElementById('calculate')
