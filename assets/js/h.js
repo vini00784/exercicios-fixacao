56010be73e670955a6a57435f8229836141ac319
Vinicius Santos <vo70346@gmail.com>
2022-08-17 11:22:36 -0300

    início exercício H

--- assets/js/h.js
+++ assets/js/h.js
@@ -1,5 +1,14 @@
 'use strict'
 
 function showNumbers() {
-    
+    const n1 = document.getElementById('number1').value
+    const n2 = document.getElementById('number2').value
+    const n3 = document.getElementById('number3').value
+    const n4 = document.getElementById('number4').value
+    const form = document.getElementById('form')
+    const showNumbers = document.getElementById('showNumbers')
+
+    if(form.reportValidity()) {
+        
+    }
 }
